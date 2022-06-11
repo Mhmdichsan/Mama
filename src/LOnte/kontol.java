1db0058e06c37f78407658087b1cc424ccd031d7
Mhmdichsan <sannn0407@gmail.com>
2022-06-11 17:18:13 +0800

    ketiga

--- src/LOnte/kontol.java
+++ src/LOnte/kontol.java
@@ -5,5 +5,6 @@ public class kontol {
     public static void main(String[] args) {
         int harga;
         System.out.println("lo kontol");
+        System.out.println("lo semua ngentot");
     }
 }
